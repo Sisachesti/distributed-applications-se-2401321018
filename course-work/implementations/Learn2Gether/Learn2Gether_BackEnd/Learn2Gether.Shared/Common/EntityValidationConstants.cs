f7ba093372d7311785f103d5a7d7ab91bcfdfe59
Sisachesti <siso95669@gmail.com>
2026-06-01 16:32:21 +0300

    Shared Class Library for storing Common Constants for the whole App

---
+++ course-work/implementations/Learn2Gether/Learn2Gether_BackEnd/Learn2Gether.Shared/Common/EntityValidationConstants.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learn2Gether.Shared.Common
+{
+    public static class EntityValidationConstants
+    {
+        public static class ModuleConstraints
+        {
+            public const int TitleMaxLength = 50;
+        }
+        public static class CourseConstraints
+        {
+            public const int TitleMaxLength = 50;
+            public const int MaxRating = 5;
+            public const int MaxDurationInMinutes = 7200;
+            public const int MaxDescription = 1000;
+            public const int ImageUrlMaxChars = 2048;
+        }
+
+        public static class LectureConstraints
+        {
+            public const int TitleMaxLength = 50;
+            public const int VideoUrlMaxLength = 2048;
+        }
+
+        public static class AnswerConstraints
+        {
+            public const int TitleMaxLength = 50;
+            public const int ContentMaxLength = 1000;
+        }
+    }
+}
